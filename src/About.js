bde3b56cc7f8a9fe47bac05cf30c76c648af807e
Alec Shears <alecshears24@gmail.com>
2020-09-13 17:26:03 -0400

    September 2020 Life Updates

--- src/About.js
+++ src/About.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './About.css';
 import HKN from './img/HKN2.jpg';
-import Headshot from './img/HKN_Headshot2.jpg';
+import Headshot from './img/Headshot.jpg';
+import oldHeadshot from './img/oldHeadshot.jpg';
 import Helpdesk from './img/Help Desk.JPG';
 import Improv from './img/Improv.jpg';
 import VN from './img/VNlogo.png';
-import Camp from './img/Camp.jpg';
+import FICS from './img/fics.jpg';
 
 
 class About extends Component{
@@ -13,25 +14,24 @@ class About extends Component{
         return(
             <div id = "About" className="About">
                 <h2>About Me</h2>
-                <p><img src={Headshot} alt="Logo" /></p>
-                <p>I am a Computer Science Student at the University of Florida Graduating in Spring of 2021.
-                    I have expereince in Java, C++, Javascript, React, Unity, C#, Assembly, and MATLAB.
+                <p><img src={oldHeadshot} alt="Logo" /></p>
+                <p>I am a Computer Science student at the University of Florida graduating in Spring of 2021.
+                    I have expereince in Java, C++, Python, Javascript, React, Unity, C#, ARM, and MATLAB.
                 </p>
                 <h2>Work Experience</h2>
-                <h3>UF Computing Help Desk</h3>
+                <h3>Florida Institute for Cybersecurity Research</h3>
+                <img src={FICS} alt="Logo" />
+                <p>As a programming intern, I designed and developed an SQL relational database based in MariaDB to store and query PCB images,
+                    developed code in Python for users to access the PCB image database quickly and easily, and used the Python library PyQt 
+                    to contribute to the development of PCB image annotation software.
+                </p>
+                <a href="https://fics.institute.ufl.edu/students/" target="_blank">View Website</a>
+                <h3>UF Computing Help Desk September 2018 - March 2020</h3>
                 <img src={Helpdesk} alt="Logo" />
-                <p>At the UF Computing Help Desk, I collaborate with coworkers to solve complex computing issues for UF Students, Faculty, and Staff.
-                    I was recently promoted to Supervisor, where I will be overseeing the entire Help Desk, ensuring everything runs smoothly and 
-                    that clients recieve the help that they deserve.
+                <p>As Supervisor at the UF Computing Help Desk, I managed a team of consultants to ensure Help Desk operations ran smoothly.
+                    As a team, we provided technical suppor to University of Florida students and staff.
                 </p>
                 <a href="http://helpdesk.ufl.edu/" target="_blank">View Website</a>
-                <h3>Temple Beth Emet Summer Camp</h3>
-                <img src={Camp} alt="Logo" />
-                <p>As a Senior Counselor for the Temple Beth Emet Summer Camp, I was in charge of overseeing children ages 3-15, making sure 
-                    that everyone was safe through events and fieldtrips. I also was in charge of leading the morning activites with a group of 300 kids and 
-                    leading an improv teambuilding elective.
-                </p>
-                <a href="https://www.templebethemet.org/camp" target="_blank">View Website</a>
                 <h2>Involvement</h2>
                 <h3>Eta Kappa Nu Honors Society</h3>
                 <img src={HKN} alt="Logo" />
@@ -44,15 +44,15 @@ class About extends Component{
                 <h3>Theatre Strike Force Improv Comedy</h3>
                 <img src={Improv} alt="Logo" />
                 <p>Theatre Strike Force is Gainesville's Premier Improv Comedy Troupe.
-                    Previously as President I worked with the board to plan events and run the club. Currently, as Vice President I am
-                    reaching out to local organizations to expand our impact and teach improv skills. We also hold an annual 12 hour
-                    comedy marathon where we raised money for the Alzheimer’s research group Hilarity for Charity.
+                    As president, I oversaw twelve board members in their respective duties to ensure projects came to fruition.
+                    and facilitated the transition to online events amidst the pandemic.
+                    I also coordinated and performed at charity shows collecting donations for Alzheimer’s research.
                 </p>
                 <a href="https://www.theatrestrikeforce.com/" target="_blank">View Website</a>
                 <h2>Research</h2>
                 <h3>The VN Project</h3>
                 <p><img src={VN} alt="Logo" /></p>
-                <p>I worked under Dr. Angelos Barmpoutis in the VN project, which is an open-source web-based client-side operating system where I
+                <p>I worked under Dr. Angelos Barmpoutis in the VN project, which is an open-source web-based client-side develppment platform where I
                     used Javascript to develop various programs to run on the platform and discover the range of possibility of the software.
                 </p>
             </div>
--- src/Experience.js
+++ src/Experience.js
@@ -3,7 +3,10 @@ import './Experience.css';
 import sharkGame from './img/Shark.png';
 import postmanGame from './img/PostMan.png';
 import turtleTank from './img/Turtle Tank.jpg';
-
+import healthTech1 from './img/HealthTech_1.gif';
+import healthTech2 from './img/HealthTech_2.gif';
+import healthTech3 from './img/HealthTech_3.gif';
+import ghostBoy from './img/ghostBoy.png';
 
 
 class Experience extends Component{
@@ -11,7 +14,24 @@ class Experience extends Component{
         return(
             <div id = "Projects" className="Experience">
                 <h2>Projects</h2>
+                <h3>UF Neurosurgery Website Project</h3>
+                <img src={healthTech1} alt="Logo" />
+                <img src={healthTech2} alt="Logo" />
+                <img src={healthTech3} alt="Logo" />
+                <p>
+                    As SCRUM Master, I managed the team’s tasks in Trello and ensured that there were no merge conflicts in Github.
+                    Together, we used the React Javascript library and MongoDB to create a webapp for the clinic and
+                    developed functionality to create and manage patient appointments with text reminders and 
+                    GPS navigation.
+                </p>
+                <a href="https://cen3031-spring2020.github.io/project-showcase" target="_blank">View Project</a>
                 <h2>Game Development</h2>
+                <h3>Ghost Boy: Ghoul Grabber</h3>
+                <img src={ghostBoy} alt="Logo" />
+                <p>I developed a 3D stealth ghost hunting game within a team environment using the Unity Game Engine.
+                    On the project, I controlled Enemy AI using scripts written in C#.
+                </p>
+                <a href="https://www.behance.net/gallery/95774371/Ghost-Boy-Ghoul-Grabber" target="_blank">Download and Play</a>
                 <h3>Lying Fish</h3>
                 <img src={sharkGame} alt="Logo" />
                 <p>This game was developed in Unity using scripts written in C#. 
